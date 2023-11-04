19716d3a765d680332e2fcefcb87a3876e40deaa
Hidetaka-Konishi <apth010f@gmail.com>
2023-11-04 17:29:21 +0900

    テストコードを元に戻す

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -12,10 +12,4 @@ end
 
 describe command('curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w "%{http_code}\n" -s') do
   its(:stdout) { should match /^200$/ }
-
-  # ポートが開いていないことをテスト
-  it { should_not be_listening }
-
-  # ステータスコードが 200 ではないことをテスト
-  it { should_not match /^200$/ }
 end
