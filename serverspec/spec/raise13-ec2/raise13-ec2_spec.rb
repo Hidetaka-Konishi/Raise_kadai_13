7e8bfd7a259fb55d1954330ced375868b27a1600
Hidetaka-Konishi <apth010f@gmail.com>
2023-11-21 15:36:22 +0900

    失敗するテストを追加してみる

--- serverspec/spec/raise13-ec2/raise13-ec2_spec.rb
+++ serverspec/spec/raise13-ec2/raise13-ec2_spec.rb
@@ -13,3 +13,7 @@ end
 describe command('curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w "%{http_code}\n" -s') do
   its(:stdout) { should match /^200$/ }
 end
+
+describe package('nginx') do
+  it { should_not be_installed }
+end
